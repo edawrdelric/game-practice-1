d70039283b024c0352489c1108a3c2b269c889cb
Kyle Kilcrease <edawrd_elric@yahoo.com>
2013-01-07 22:48:15 -0800

    New code

--- sample.rb
+++ sample.rb
@@ -20,6 +20,7 @@ start_color()
 cbreak()
 curs_set(0)
 noecho
+stdscr.keypad(true)
 
 char_x = 1
 char_y = 1
@@ -31,27 +32,26 @@ game_map = Array.new(map_width) {Array.new(map_height){2}}
 
 while true
 
-	keypress = getch()
-
-	if keypress == "s"
+	case Curses.getch()
+	when Curses::Key::DOWN
 		if char_y < map_height-1
 			char_y += 1
 		else
 			char_y = 0
 		end
-	elsif keypress == "d"
+	when Curses::Key::RIGHT
 		if char_x < map_width-1
 			char_x += 1
 		else
 			char_x = 0
 		end
-	elsif keypress == "a"
+	when Curses::Key::LEFT
 		if char_x > 0
 			char_x -= 1
 		else
 			char_x = map_width-1
 		end
-	elsif keypress == "w"
+	when Curses::Key::UP
 		if char_y > 0
 			char_y -= 1
 		else
